4b16aae592c60982462628bd1afb3be92c68ce2a
Arafath <rsarafath.signin@gmail.com>
2026-04-07 00:39:43 +0600

    I make button togleing

--- src/Componants/players/Players.jsx
+++ src/Componants/players/Players.jsx
@@ -1,26 +1,45 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailaablePlayers from '../HomePage/banner/availablePlayers/AvailaablePlayers';
+import SelectedPlayers from '../HomePage/SelectedPlayers/SelectedPlayers';
 
 const Players = ({ playersPromise }) => {
   const players = use(playersPromise);
+  const [selectedType,setSelectedType]=useState('available')
 
   return (
     <div className="container mx-auto">
       <div className="flex justify-between gap-4 items-center">
-        <h2 className="font-bold text-3xl">
-          Available Players: {players.length}
-        </h2>
-
+        {
+          selectedType === 'available' ? (
+          <h2 className="font-bold text-3xl">
+            Available Players: {players.length}
+          </h2>
+        ) : (
+          <h2 className="font-bold text-3xl">
+            Selected Players: {}
+          </h2>
+        )}
+        ;
         <div>
-          <button className="btn  bg-[#e7fe29] rounded-r-none rounded-l-xl ">
+          <button
+            onClick={() => {
+              setSelectedType('available');
+            }}
+            className={`btn ${selectedType === 'available' ? 'bg-[#e7fe29] font-bold' : ''} rounded-r-none rounded-l-xl`}
+          >
             Availavle
           </button>
-          <button className="btn rounded-l-none rounded-r-xl font-bold">
-            Selected
+          <button
+            onClick={() => {
+              setSelectedType('selected');
+            }}
+            className={`btn ${selectedType === 'selected' ? 'bg-[#e7fe29] font-bold' : ''} rounded-l-none rounded-r-xl `}
+          >
+            Selected (0)
           </button>
         </div>
       </div>
-      <AvailaablePlayers players={players}></AvailaablePlayers>
+      {selectedType==='available' ?  <AvailaablePlayers players={players}></AvailaablePlayers> : <SelectedPlayers></SelectedPlayers>}
     </div>
   );
 };
